c9d748ee27c46d1fa16d2be42a6b00ff7b550692
andrea <andrea@192.168.20.104>
2018-04-16 15:56:35 +0200

    34esimo commit: messo a view la dialog per la ricerca veicoli     - il refactoring della selezione dei lavori standard come select 	checkbox. 	- la modifica dei dati del veicolo e del cliente del veicolo. 	- La stampa della pratica e relativi lavori e materiali 	- LA VISUALIZZAZIONI DELLE N PRATICHE DA UNA CERTA DATA ( 	per il momento vengono visualizzati solo gli ultimi N con 	attualmente N=20.

---
+++ src/main/java/com/javasoft81/pratichemanager/jsf/beans/utils/SearchVeicoliDialog.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javasoft81.pratichemanager.jsf.beans.utils;
+
+import com.javasoft81.pratichemanager.entities.Cliente;
+import com.javasoft81.pratichemanager.entities.Veicolo;
+import com.javasoft81.pratichemanager.jsf.beans.VeicoliSearchView;
+import java.util.List;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+
+/**
+ *
+ * @author andrea
+ */
+public class SearchVeicoliDialog {
+    
+    private List<Veicolo> filteredVeicoli;
+    
+    private VeicoliSearchView v;
+    
+    private List<Veicolo> veicoli;
+
+
+    /**
+     * Creates a new instance of SearchVeicoliDialog
+     */
+    public SearchVeicoliDialog() {
+        
+    }
+    
+    @PostConstruct
+    public void init(){
+        FacesContext context = FacesContext.getCurrentInstance();     
+        v = context.getApplication().evaluateExpressionGet(context, "#{veicoliSearchView}", VeicoliSearchView.class);
+        veicoli = v.getVeicoli();
+    }
+
+    public List<Veicolo> getFilteredVeicoli() {
+        return filteredVeicoli;
+    }
+
+    public void setFilteredVeicoli(List<Veicolo> filteredVeicoli) {
+        this.filteredVeicoli = filteredVeicoli;
+    }    
+
+    public List<Veicolo> getVeicoli() {
+        return veicoli;
+    }
+
+    public void setVeicoli(List<Veicolo> veicoli) {
+        this.veicoli = veicoli;
+    }
+    
+    public boolean filterVeicoloByCliente(Object value, Object filter, Locale locale) {
+        String filterText = (filter == null) ? null : filter.toString().trim();
+        if(filterText == null||filterText.equals("")) {
+            return true;
+        }         
+        if(value == null) {
+            return false;
+        }
+        String nome = ((Cliente)value).getNome()==null?"":((Cliente)value).getNome();
+        String cognome = ((Cliente)value).getCognome()==null?"":((Cliente)value).getCognome();
+        boolean result;
+        result = (nome.contains(filterText) || cognome.contains(filterText));
+        return result;
+    }
+    
+    
+    public void selectCarFromDialog(Veicolo veicolo){
+        RequestContext.getCurrentInstance().closeDialog(veicolo);
+    }
+    
+    
+}
